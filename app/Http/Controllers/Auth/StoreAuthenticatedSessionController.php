be03bcbd5481dcc7761330771aea00842a2b1349
ryoki <ryoki.tsuji@gmail.com>
2024-08-22 18:03:02 +0900

    ログイン（ユーザーもストアも）

---
+++ app/Http/Controllers/Auth/StoreAuthenticatedSessionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\StoreLoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class StoreAuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.store-login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(StoreLoginRequest $request): RedirectResponse
+    {
+        // `web`ガードではなく、`store_user`ガードを使用する
+        if (Auth::guard('store_user')->attempt($request->only('email', 'password'), $request->filled('remember'))) {
+            $request->session()->regenerate();
+
+            session()->flash('success', 'ログインしました!');
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        return back()->withErrors([
+            'email' => 'ログイン情報が正しくありません。',
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('store_user')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        session()->flash('success', 'ログアウトしました!');
+
+        return redirect('/');
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\StoreAuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
@@ -33,6 +34,11 @@ Route::middleware('guest')->group(function () {
                 ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('store-login', [StoreAuthenticatedSessionController::class, 'create'])
+                ->name('store-login');
+
+    Route::post('store-login', [StoreAuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
