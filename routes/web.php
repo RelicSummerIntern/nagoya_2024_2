583b865e3120032c65fee2443eee7bcb73118061
haruto8610 <haruto.fukushina0207@gmail.com>
2024-08-22 16:30:11 +0900

    pullしたい

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::get("/template/stores", function (){
 
     Route::get('/quest_board', [QuestController::class, 'index'])->name('Quest.index');
     Route::get('/storesmypage/{id}', [StoreController::class, 'index'])->name('Store.index');
+    Route::get('/users-storespage', [StoreController::class, 'show'])->name('Store.show');
 
 Route::get('/users-storespage', function () {
     return view('users-storespage');
