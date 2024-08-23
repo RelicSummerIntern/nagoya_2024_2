36232f35125f635edf119ba3ca7d15bfa5f0863d
fuwafuwa1109 <jy.02b.1758@s.thers.ac.jp>
2024-08-23 13:19:43 +0900

    おすすめ修正

--- resources/views/recommend.blade.php
+++ resources/views/recommend.blade.php
@@ -7,10 +7,11 @@
         <title>今日のオススメ</title>
         <style>
             body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f4;
+                margin: 0;
+                padding: 0;
+                text-align: center;
             }
             .recent_open {
                 margin: 40px 0;
@@ -20,15 +21,17 @@
                 font-weight: bold;
                 border-radius: 10px; /* 角を少し丸くする */
                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* 影を追加して立体感を出す */
+                max-width: 800px;
+                margin: 50px auto; /* フォーム全体を少し下に下げる */
+                text-align: center;
             }
             .container {
-            display: grid;
             grid-template-columns: 1fr 1fr;
             }
             @media (max-width: 782px){
                 .container{
                 display: flex;
-                flex-direction: column;
+                flex-direction: row;
                 }
             }
             .header, .footer {
@@ -37,7 +40,7 @@
             }
             .recomend_text {
                 text-align: left;
-                font-size: 80px
+                font-size: 30px
             }
             .return {
                 width: 960px;
@@ -62,7 +65,7 @@
             <div class="header">
                 <img src="images/sample.jpg" alt="店舗画像">
             </div>
-            <div class="rocomend_text">
+            <div class="recomend_text">
                 <ol>
                     <li>店舗名:</li>
                     <li>ジャンル:</li>
@@ -72,10 +75,11 @@
                 </ol>
             </div>
         </div>
-        <a href="{{ route('Quest.index') }}" class="return">
-            クエストボードに移動
-        </a>
+        
     </div>
+    <a href="{{ route('Quest.index') }}" class="return">
+        クエストボードに移動
+    </a>
 </body>
 
 @endsection
